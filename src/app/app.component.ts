9c2f6b9f963fd08014b28d626269cef4bb0e9ca5
Yash Gaur <ygaur697@gmail.com>
2019-06-18 13:10:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,15 +39,54 @@ export class AppComponent implements OnInit {
      value3: 10,
    }]*/
   public barChartData = [];
-  norm = {
-    id: 1,
-    label: 'label',
-    value1: 10
-  };
 
   ngOnInit() {
-    const a  = [1, 2, 3, 4, 5];
+    const a = [1, 2, 3, 4, 5];
     console.log(a);
+    console.log("im getting called ");
+
+    this.chartArray = new Chart('bar', {
+      type: 'bar',
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ],
+          xAxes: [
+            {
+              display: true,
+              gridLines: {
+                display: false
+              },
+
+              labels: ['a', 'b', 'c', 'd', 'e'],
+            }
+          ]
+        },
+        title: {
+          display: true,
+          text: 'Bar '
+        }
+      },
+      data: {
+        //labels: values,
+        datasets: [
+          {
+            type: 'bar',
+            label: 'My First dataset',
+            data: this.barChartData,
+            backgroundColor: 'rgba(255,0,255,0.4)',
+            borderColor: 'rgba(255,0,255,0.4)',
+            fill: false
+          }
+        ]
+      }
+    });
 
     //Here we will listen to an event from the socket io server
 
@@ -69,16 +108,26 @@ export class AppComponent implements OnInit {
     console.log('VAL', this.startVal);
     this.webSocketService.emit('test', values);
     this.webSocketService.listen('response').subscribe(data => {
-      // console.log(data);
+      console.log("received response");
+      console.log("This is what we are looking at", data);
+
       this.updategraph(data);
+
+
+
+
       this.displayData = data;
       console.log(this.displayData);
     });
 
 
   }
-  updategraph(values) {
+  updategraph(data) {
     this.barChartData = [];
+    this.chartArray.destroy();
+    this.ngOnInit();
+
+
     // for (let i = 0; i < values.length; i++) {
     //   this.norm = {
     //     id: i,
@@ -87,37 +136,42 @@ export class AppComponent implements OnInit {
     //   };
     //   this.barChartData.push(this.norm);
     // }
-    this.chartArray = new Chart('bar', {
-      type: 'bar',
-      options: {
-        responsive: true,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        },
-        title: {
-          display: true,
-          text: 'Bar '
-        }
-      },
-      data: {
-        labels: ['a', 'b', 'c', 'd', 'e'],
-        datasets: [
-          {
-            type: 'bar',
-            label: 'My First dataset',
-            data: values,
-            backgroundColor: 'rgba(255,0,255,0.4)',
-            borderColor: 'rgba(255,0,255,0.4)',
-            fill: false
-          }
-        ]
-      }
-    });
+    for (let x = 0; x < data.length; x++) {
+      console.log("value of x outsite", x);
+      setTimeout(() => {
+
+        this.chartArray.data.datasets.forEach((dataset) => {
+          dataset.data.push(data[x]);
+          console.log(dataset.data)
+          console.log("value inside timeout", x);
+          console.log(this.chartArray.data.datasets[0].data);
+
+        });
+
+
+        this.chartArray.update();
+
+      }, 1000 * (x + 1));
+      var add = this.chartArray.data.datasets[0].data;
+      let z = (add) => add.filter((v, i) => add.indexOf(v) === i)
+      console.log("value of x", z);
+
+    }
+    console.log("outside for");
+    console.log("without filter", this.chartArray.data.datasets[0].data);
+
+
+
   }
+
+  /*removedata(data) {
+    this.chartArray.data.labels.pop();
+    this.chnaaddmesartArray.data.datasets.forEach((dataset) => {
+      dataset.data.pop();
+    });
+    this.chartArray.update();
+  }*/
+
+
+
 }
